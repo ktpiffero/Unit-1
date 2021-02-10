5e67323dcca3ee2b38eb8225df3bea9acd5102d5
Kevin <ktpiffero@aol.com>
2021-02-10 15:18:17 -0800

    Final commit

---
+++ Unit_1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Unit_1
+//
+//  Created by Kevin Piffero on 2/10/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
